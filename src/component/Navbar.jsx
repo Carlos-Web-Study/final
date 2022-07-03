ee501c1d47b5cefe1129959f7062ce6088c1be7b
Carlos Montejo <montejocruzcarlos@gmail.com>
2022-07-02 20:17:28 -0500

    Updated

--- src/component/Navbar.jsx
+++ src/component/Navbar.jsx
@@ -7,7 +7,7 @@ const Navbar = () => {
 shadow-sm">
                 <div class="container">
                     <a class="navbar-brand fw-bold fs-4" href="#">
-                        LA COLLECTION 
+                        HALTHY DRINKS 
                     </a>
                     <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
                         <span class="navbar-toggler-icon"></span>
