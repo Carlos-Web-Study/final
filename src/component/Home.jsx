e914060b545b8c15b55ffad6260641e094d38459
Carlos Montejo <montejocruzcarlos@gmail.com>
2022-07-04 17:11:20 -0500

    Updated

--- src/component/Home.jsx
+++ src/component/Home.jsx
@@ -2,12 +2,16 @@ import React from "react";
 
 const Home = () => {
     return (
-        <div class="card bg-dark text-white">
-            <img src="..." class="card-img" alt="..." />
-            <div class="card-img-overlay">
-                <h5 class="card-title">Card title</h5>
-                <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
-                <p class="card-text">Last updated 3 mins ago</p>
+        <div class="card bg-dark text-white border-0">
+            <img src="/drinks/hd.jpg" class="card-img" alt="Background" 
+            height='550px'/>
+            <div class="card-img-overlay d-flex flex-column
+            justify-content-center">
+                <div className="container">
+                <h5 class="card-title display-3 fw-bolder mb-0">HILTHY DRINKS</h5>
+                <p class="card-text">bienvenidos.
+                </p>
+                </div>
             </div>
         </div>
     );
